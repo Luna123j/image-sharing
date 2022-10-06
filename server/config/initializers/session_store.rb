be89c9e29c85c1001fc2a78d5a1c2178c8420f86
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-06 12:20:40 -0400

    setup users in beckend

---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+# if Rails.env === 'production' 
+#   Rails.application.config.session_store :cookie_store, key: '_image-sharing', domain: 'server'
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_image-sharing'
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/users',         to: 'users#create'
+  get '/users/:user_id', to: 'users#show'
+  get '/users',          to: 'users#index'
+
+  
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :cats, only: [:index]
+  resources :users, only: [:create, :show, :index] do 
+    resources :items, only: [:create, :show, :index, :destroy]
+  end
 end
