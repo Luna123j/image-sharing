8f6dc6f6d48abe35078a5e776ecaf7b439bc4a8c
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-07 13:49:57 -0400

    add pictures model and create test data

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_160858) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_164817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_160858) do
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "description"
+    t.string "img"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,10 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Creating Cats!"
-Cat.destroy_all
+puts "Creating Pictures!"
+User.destroy_all
+Picture.destroy_all
 
-Cat.create(name: 'Puma', img: 'https://2static.fjcdn.com/pictures/Derp+cat_9720f6_6005436.jpg')
-Cat.create(name: 'Rosy', img: 'https://i.redd.it/6xvh1f7btgl31.jpg')
-Cat.create(name: 'Mr Buttons', img: 'https://live.staticflickr.com/1614/25492212465_7ff33b58bb_b.jpg')
-Cat.create(name: 'Luna', img: 'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg')
+User.create(username: 'test', email: 'test@test.com', password_digest: '$2a$04$kIOSD2KxSLqXa7ht1SoNaOinzwWU5V/gto6bXkj.9ahR96GeM4dBC')
+
+Picture.create(description: 'Puma', img: 'https://2static.fjcdn.com/pictures/Derp+Picture_9720f6_6005436.jpg',user_id: 1)
+Picture.create(description: 'Rosy', img: 'https://i.redd.it/6xvh1f7btgl31.jpg',user_id: 1)
+Picture.create(description: 'Mr Buttons', img: 'https://live.staticflickr.com/1614/25492212465_7ff33b58bb_b.jpg',user_id: 1)
+Picture.create(description: 'Luna', img: 'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg',user_id: 1)
