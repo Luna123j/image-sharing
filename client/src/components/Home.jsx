21294cc062e8bf8640375c86459ef2a135e8c89a
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-11 15:03:26 -0400

    edit popup window

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react"
 import NavBar from "./NavBar";
+import './Home.scss'
 import UploadImage from "./UploadImage";
 const Home = () => {
 
   const [popup, setpopup] = useState(false)
   const submitHandler = () => {
-    setpopup(true);
-    { popup ? <UploadImage /> : null }
+    setpopup(!popup);
+
   }
 
   return (
-    <div>
-      <form onSubmit={submitHandler}>
+    <div className="home">
+      <div className="btn" onClick={submitHandler}>
         <button type='submit'>Share</button>
-      </form>
-
+      </div>
+      {popup ? <UploadImage toggle={submitHandler}/> : null}
+      
     </div>
   )
 }
--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
-
-const UploadImage = () => {
+import './UploadImage'
+const UploadImage = (props) => {
   const [selectedImage, setSelectedImage] = useState(null);
   const imageSubmitHandler = () => {
 
   }
 
-  return (
-    <div>
-      {selectedImage && (
-        <div>
-          <form onSubmit={imageSubmitHandler}>
-            <img alt="error showing image" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-            <br />
-            <button onClick={() => setSelectedImage(null)}>Remove</button>
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      )}
-      <br />
+  const handleClick = () => {
+    props.toggle();
+  };
 
-      <br />
-      <input
-        type="file"
-        name="myImage"
-        onChange={(event) => {
-          setSelectedImage(event.target.files[0]);
-        }}
-      />
+  return (
+    <div className="popup-box">
+      <div className="box">
+        <span className="close-icon" onClick={handleClick}>X   </span>
+        <p>I'm A Pop Up!!!</p>
+        {selectedImage && (
+          <div>
+            <form onSubmit={imageSubmitHandler}>
+              <img alt="error showing image" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+              <br />
+              <button onClick={() => setSelectedImage(null)}>Remove</button>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        )}
+        <br />
+
+        <br />
+        <input
+          type="file"
+          name="myImage"
+          onChange={(event) => {
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+
+      </div>
     </div>
+
   )
 }
 
